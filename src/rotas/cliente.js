d7b39f1c9b498c0905507ac33e1bf867224b7f65
Samuel Lopes <samuellopes12390@gmail.com>
2023-05-23 23:38:52 -0300

    Feito a necessidade de o usuário está autenticado para acessar as páginas

--- src/rotas/cliente.js
+++ src/rotas/cliente.js
@@ -2,52 +2,82 @@ import { Router } from "express";
 
 import { buscarCarroPorId, buscarUltimosAlugueisPorCarro } from "../casos-de-uso/index.js";
 import { repositorioVeiculo } from "../repositorios/index.js";
-import { repositorioUsuario } from "../repositorios/RepositorioUsuario.js";
 
 const rotasCliente = Router();
 
 rotasCliente.get("/loja/conta", (request, response) => {
-  response.render("cliente/cliente-conta", {})
+  if(request.session.usuario){
+    let usuarioSession = request.session.usuario;
+    response.render("cliente/cliente-conta", { usuario: usuarioSession[0] });
+  }else{
+    response.redirect("/signin");
+  }
 })
 
 rotasCliente.post("/loja/conta", (request, response) => { // Atualizar dados conta usuario
-  response.render("cliente/cliente-conta", {})
+  if(request.body.senha){ // Caso a requisição venha para alterar senha
+    
+  }else{ // Caso a requisição venha para editar os dados do usuário
+
+  }
+  response.render("cliente/cliente-conta", { usuario: {} })
 })
 
 rotasCliente.get("/loja/conta-editar", (request, response) => {
-  response.render("cliente/cliente-editar-conta", {})
+  if(request.session.usuario){
+    let usuarioSession = request.session.usuario;
+    response.render("cliente/cliente-editar-conta", { usuario: usuarioSession[0] });
+  }else{
+    response.redirect("/signin");
+  }
 })
 
 rotasCliente.get("/loja/conta-senha", (request, response) => {
-  response.render("cliente/cliente-senha-conta", {})
+  if(request.session.usuario){
+    response.render("cliente/cliente-senha-conta", {});
+  }else{
+    response.redirect("/signin");
+  }
 })
 
 rotasCliente.get("/loja/alugar/:idCarro", (request, response) => {
-  const idCarro = request.params.idCarro;
-  const carroSelecionado = buscarCarroPorId(idCarro);
+  if(request.session.usuario){
+    const idCarro = request.params.idCarro;
+    const carroSelecionado = buscarCarroPorId(idCarro);
 
-  if(!carroSelecionado){
-    const mensagem = "Erro ao encontrar carro selecionado para aluguel";
-    response.redirect(`/erro?mensagem=${mensagem}`);
-  }
-
-  const ultimosAlugueisCarro = buscarUltimosAlugueisPorCarro(idCarro);
-  
-  response.render("cliente/solicitacao-aluguel", 
-    { 
-      carroSelecionado, 
-      ultimosAlugueis: ultimosAlugueisCarro 
+    if(!carroSelecionado){
+      const mensagem = "Erro ao encontrar carro selecionado para aluguel";
+      response.redirect(`/erro?mensagem=${mensagem}`);
     }
-  );
+
+    const ultimosAlugueisCarro = buscarUltimosAlugueisPorCarro(idCarro);
+  
+    response.render("cliente/solicitacao-aluguel", 
+      { 
+        carroSelecionado, 
+        ultimosAlugueis: ultimosAlugueisCarro 
+      }
+    );
+  }else{
+    response.redirect("/signin");
+  }
 });
 
 rotasCliente.get("/loja", async (request, response)=> {
-  const ListaDeveiculos = await repositorioVeiculo.pegarVeiculos();
-  response.render("cliente/cliente-loja", {veiculos: ListaDeveiculos});
+  if(request.session.usuario){
+    const ListaDeveiculos = await repositorioVeiculo.pegarVeiculos();
+    response.render("cliente/cliente-loja", {veiculos: ListaDeveiculos});
+  }else{
+    response.redirect("/signin");
+  }
 });
 
 rotasCliente.get("/loja/aluguel", (request, response)=>{
-  response.render("cliente/cliente-aluguel");
+  if(request.session.usuario){
+    response.render("cliente/cliente-aluguel");
+  }else{
+    response.redirect("/signin");
+  }
 });
 
 
